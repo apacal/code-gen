4299322bfd1f1dd33722a1dce3dd7e444d4068a0
apacalzhong <apacalzhong@tencent.com>
2016-03-03 20:59:05 +0800

    add readme.md

---
+++ README.md
@@ -0,0 +1,4 @@
+#code-gen
+code-gen是一个复合项目，包含code-gen-java，code-gen-cpp用java或者cpp来进行文本模板渲染，生成对应的文本文件。
+使用XML定义数据，再根据模版文件渲染文本。
+目前完成java部分
--- code-gen-java/src/cn/apacal/codegen/Main.java
+++ code-gen-java/src/cn/apacal/codegen/Main.java
@@ -5,7 +5,8 @@ public class Main {
 
     /**
      * 使用velocity来生成文件,可以是java源码, cpp源码等
-     * java -jar templatePath templateName xmlPath outPath
+     * java -jar genType templatePath templateName xmlPath outPath
+     * genType 使用何种方式
      * templatePath velocity的模版目录路径
      * templateName 模版文件名
      * xmlPath java读取数据的xml文件路径
@@ -20,17 +21,21 @@ public class Main {
 //        String outPath = "/Users/apacalzhong/src/code-gen/code-gen-java/build";
 
         if (args.length < 4) {
-            System.out.println("args less, must four args, [templatePath templateName xmlPath outPath]");
+            System.out.println("args less, must five args, [genType templatePath templateName xmlPath outPath]");
+            System.out.println("genType: as[db]");
             return;
         }
 
-        String templatePath = args[0];
-        String templateName = args[1];
-        String xmlPath = args[2];
-        String outPath = args[3];
+        String genType = args[0];
+        String templatePath = args[1];
+        String templateName = args[2];
+        String xmlPath = args[3];
+        String outPath = args[4];
 
         CodeGen codeGen = new CodeGen();
-        codeGen.genDBFile(templatePath, templateName, xmlPath, outPath);
+        if (genType.equals("db")) {
+            codeGen.genDBFile(templatePath, templateName, xmlPath, outPath);
+        }
 
 
 
